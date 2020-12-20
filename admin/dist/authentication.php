abdf1c68bbac00ceb8a63ce2e7e68d4064ae7556
astropsumit <cosmictunnel376@gmail.com>
2020-12-20 15:39:56 +0530

    Changes

--- admin/dist/authentication.php
+++ admin/dist/authentication.php
@@ -1,26 +1,26 @@
-<?php      
+<?php
     session_start();
-    include('connection.php');  
-    $username = $_POST['user'];  
-    $password = $_POST['pass'];  
-      
-        //to prevent from mysqli injection  
-        $username = stripcslashes($username);  
-        $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($con, $username);  
-        $password = mysqli_real_escape_string($con, $password);  
-      
-        $sql = "select *from login where username = '$username' and password = '$password'";  
-        $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);  
-          
+    include('connection.php');
+    $username = $_POST['user'];
+    $password = $_POST['pass'];
+
+        //to prevent from mysqli injection
+        $username = stripcslashes($username);
+        $password = stripcslashes($password);
+        $username = mysqli_real_escape_string($con, $username);
+        $password = mysqli_real_escape_string($con, $password);
+
+        $sql = "select *from login where username = '$username' and password = '$password'";
+        $result = mysqli_query($con, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+
         if($count == 1){
             $_SESSION['loggedin'] = true;
-            header("Location:http://techno.birlainstitute.co.in/certificate/make", true, 301);
+            header("Location: /certificate/make", true, 301);
             exit();
-        }  
-        else{  
-            echo "<h1> Login failed. Invalid username or password.</h1>";  
-        }     
-?>  
+        }
+        else{
+            echo "<h1> Login failed. Invalid username or password.</h1>";
+        }
+?>
